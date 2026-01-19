8bca061937004e0d0372d3903ba821ba64d30bfc
JeongwooSeo <shipfriend4371@naver.com>
2026-01-19 23:31:24 +0900

    fix: 파일 삭제 시 필드 초기화를 deleteField()로 변경

--- src/Components/RecruitFileUpload.tsx
+++ src/Components/RecruitFileUpload.tsx
@@ -190,7 +190,7 @@ const RecruitFileUpload = () => {
           docRef,
           {
             ...currentData,
-            [`${type}File`]: undefined,
+            [`${type}File`]: deleteField(),
             updatedAt: new Date().toISOString(),
           },
           { merge: true },
